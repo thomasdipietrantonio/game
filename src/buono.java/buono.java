838354e4f7b16d6fd9d3ae9eab74cc7f7e0bfc2a
Thomas <thomas.dipietrantonio@liceovittoriacolonnaroma.edu.it>
2026-05-13 13:46:18 +0200

    Add characters and buono classes with basic attributes and methods

---
+++ src/buono.java/buono.java
@@ -0,0 +1,27 @@
+public buono extends characters {
+    
+    private int forza;
+
+    
+    public buono(String nome, int vita, int f) {
+        super(nome, vita);
+        this.forza = f;
+    }
+
+
+
+
+    public int getForza() {
+        return forza;
+    }
+
+
+    
+
+
+
+
+
+
+
+}
---
+++ src/characters.java
@@ -0,0 +1,28 @@
+public class characters {
+    
+
+String nome;
+int vita;
+
+public characters(String nome, int vita) {
+    this.nome = nome;
+    this.vita = vita;
+
+}
+
+public String getNome() {
+    return nome;
+}
+public int getVita() {
+    return vita;
+}
+
+
+
+
+
+
+
+
+
+
